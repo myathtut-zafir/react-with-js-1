21e7927aed75c65e4697806fa4115d22bc7e2996
myathtut-zafir <aa.htut@gmail.com>
2026-02-28 15:22:34 +0700

    feat: add tailwand

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(`${title} has been liked ${hasLiked}`);
-    // console.log(`${title} has been liked ${count}`);
-  }, [hasLiked]);
-
+const App = () => {
   return (
-    <div
-      className="card"
-      onClick={() => setCount((prevState) => prevState + 1)}
-    >
-      <h2>
-        {title} - {count || null}
-      </h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "Liked" : "Like"}
-      </button>
+    <div>
+      <h1 class="text-3xl font-bold underline text-red-900">Hello world!</h1>
     </div>
   );
 };
-function App() {
-  return (
-    <div className="card-container">
-      <Card title="A" />
-      <Card title="B" />
-      <Card title="C" />
-    </div>
-  );
-}
 
 export default App;
