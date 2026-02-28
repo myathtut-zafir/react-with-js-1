82717203d17ff9bb60ba968f3638ded11ad760ad
myathtut-zafir <aa.htut@gmail.com>
2026-02-28 11:17:51 +0700

    fix:  inline style to external css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
 
+
 const Card = ({ title }) => {
   return (
-    <div style={{
-      border:'1px solid #4b5362',
-      padding:'20px',
-      margin:'10px',
-      backgroundColor:'#31363f',
-      borderRadius:'10px',
-      minHeight:'100px'
-      }}>
-      <h2>{title}</h2>
+    <div className="card">
+      <h2>
+        {title}
+      </h2>
     </div>
   );
 };
