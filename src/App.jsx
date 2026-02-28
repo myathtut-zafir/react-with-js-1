00b45d9758cad46e00bcab94f6dacffbee5c3063
myathtut-zafir <aa.htut@gmail.com>
2026-02-28 11:49:41 +0700

    feat:  use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(`${title} has been liked ${hasLiked}`);
+    // console.log(`${title} has been liked ${count}`);
+  }, [hasLiked]);
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div
+      className="card"
+      onClick={() => setCount((prevState) => prevState + 1)}
+    >
+      <h2>
+        {title} - {count || null}
+      </h2>
       <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked?'Liked':"Like"}
-        </button>
+        {hasLiked ? "Liked" : "Like"}
+      </button>
     </div>
   );
 };
